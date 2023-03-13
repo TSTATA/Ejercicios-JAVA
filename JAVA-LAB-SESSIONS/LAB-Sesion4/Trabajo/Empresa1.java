280684db747ff468d11b42bcdd8d629daf59336d
TAYEBI TAYEBI Amine <amine.tayebi@soprabanking.com>
2023-03-13 13:19:19 +0100

    Correccion

--- JAVA-LAB-SESSIONS/LAB-Sesion4/Trabajo/Empresa1.java
+++ JAVA-LAB-SESSIONS/LAB-Sesion4/Trabajo/Empresa1.java
@@ -1,5 +1,5 @@
 // TSTATATRABAJO
-6  public class Empresa1 implements Trabajo {
+   public class Empresa1 implements Trabajo {
   	//Esta empresa paga a 12 euros la hora de trabajo
   	public double pagarSueldo(double horas) 	{
   		return horas*12;
