f6ab4487483627a6ac1f1263fb2845a448ca863c
TAYEBI TAYEBI Amine <amine.tayebi@soprabanking.com>
2023-03-13 13:13:44 +0100

    TSTATATRABAJO: IMPLEMENTS

--- JAVA-LAB-SESSIONS/LAB-Sesion4/Trabajo/Empresa2.java
+++ JAVA-LAB-SESSIONS/LAB-Sesion4/Trabajo/Empresa2.java
@@ -1,6 +1,7 @@
-  public class Empresa2 implements Trabajo {	
+// TSTATATRABAJO
+  public class Empresa2 implements Trabajo {
   	//Esta empresa paga a 15 euros la hora de trabajo
   	public double pagarSueldo(double horas) 	{
-  		return horas*15; 
+  		return horas*15;
   	}
   }
