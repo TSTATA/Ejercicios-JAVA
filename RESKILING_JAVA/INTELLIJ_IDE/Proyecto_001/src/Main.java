297eb4c43af0d77ebf5e4272572dbd499a1d5837
TAYEBI TAYEBI Amine <amine.tayebi@soprabanking.com>
2023-07-06 10:55:09 +0200

    comentarios y variabes, tipos de datos etc

---
+++ RESKILING_JAVA/INTELLIJ_IDE/Proyecto_001/src/Main.java
@@ -0,0 +1,19 @@
+/*
+Practico tambien sobre el Packages. sirve para organizar y hay un estandar para nombrar.
+los nombres de los paquetes tienen que ir en minuscula.
+
+ */
+//package ejercicios.ejerciciouno.Main;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hay tres tipos de comentarios en java  " );
+        System.out.println("// line comments  ");
+        System.out.println("/*...*/ block comments  ");
+        /*    block comments
+        * dkd d
+        * dkdk
+        * System.out.println("block comments comments  ");
+        *  */
+        System.out.println("/**...*/ JavaDoc comments. genera documentación fuera del alcance del curso   ");
+    }
+}
